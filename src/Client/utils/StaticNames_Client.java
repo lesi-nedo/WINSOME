85db0496101b0e66fe5c3fdf5b699a30cac29913
lesi-nedo <oleksiy12345@live.it>
2022-01-06 01:07:53 +0100

    deleted unimportant files

--- src/Client/utils/StaticNames_Client.java
+++ src/Client/utils/StaticNames_Client.java
@@ -9,6 +9,6 @@ public class StaticNames_Client {
 	public static final String PATH_TO_FILE_FOLL="/WINSOME/src/Client/"; // path to the json file where all followers will be  stored
 	public static final String NAME_FILE_FOLL="/followers.json";//name of the json file where all followers of the client x will be stored
 	public static final String NAME_CONF_FILE="src/Client/conf_file.txt";
-	public static final String PATH_TO_CLIENT="src/Client/";
+	public static final String PATH_TO_CLIENT="src/Client/Users/";
 	public static final JsonEncoding ENCODING=JsonEncoding.UTF16_BE;
 }
